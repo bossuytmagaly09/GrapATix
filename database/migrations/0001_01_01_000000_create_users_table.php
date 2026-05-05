661ba5761b3d66dc939a8e021b15c4ce888e02fb
Magaly Bossuyt <bossuytmagaly2@gmail.com>
2026-05-05 15:43:08 +0200

    Fix tests by enabling RefreshDatabase and adding Organization model/factory

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('organization_id')->constrained()->onUpdate('cascade')->onDelete('restrict');
+            $table->foreignId('organization_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('restrict');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
